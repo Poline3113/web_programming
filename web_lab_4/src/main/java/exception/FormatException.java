1e3e0e4de43e2cd253808c6cab2a910c82ac93de
Poline3113 <polinapetrova76232@gmail.com>
2026-02-18 21:47:23 +0300

    Delete web_lab_4 directory

--- web_lab_4/src/main/java/exception/FormatException.java
+++
@@ -1,7 +0,0 @@
-package exception;
-
-public class FormatException extends Exception {
-    public FormatException(String message) {
-        super(message);
-    }
-}
--- web_lab_4/src/main/java/rest/response/SessionStatus.java
+++
@@ -1,9 +0,0 @@
-package rest.response;
-
-public  class SessionStatus {
-    public boolean authenticated;
-
-    public SessionStatus(boolean authenticated) {
-        this.authenticated = authenticated;
-    }
-}
